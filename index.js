8ac93684ca5a76abd2de26ad3d09241c66b33a68
Alex Blom <alex@alexblom.com>
2017-09-12 15:32:39 -0400

    init

---
+++ index.js
@@ -0,0 +1,35 @@
+const path               = require('path');
+const glob               = require('glob');
+const SingleEntryPlugin  = require('webpack/lib/SingleEntryPlugin');
+const cordovaAssetTree   = require('corber/lib/targets/cordova/utils/cordova-assets');
+
+function CorberWebpackPlugin () {};
+
+CorberWebpackPlugin.prototype.apply = function(compiler) {
+  let context = compiler.context;
+  let platform = process.argv.includes('--CORBER_PLATFORM=android')? 'android' : 'ios';
+  let cdvAssets = cordovaAssetTree.getPaths(platform, './corber/cordova');
+
+  cdvAssets.files.forEach((file) => {
+    if (file === 'plugins/**') {
+      //need to build the tree until corber/cordova/utils/cordova-assets is upgraded
+      let plugins = glob.sync(path.join('./corber/cordova', cdvAssets.assetsPath, 'plugins/**/*.js'));
+      plugins.forEach((plugin) => {
+        compiler.apply(new SingleEntryPlugin(
+          context,
+          path.join(process.cwd(), plugin),
+          plugin
+        ));
+      });
+    } else {
+      let filePath = path.join(process.cwd(), './corber/cordova', cdvAssets.assetsPath, file);
+      compiler.apply(new SingleEntryPlugin(
+        context,
+        filePath,
+        file
+      ));
+    }
+  });
+};
+
+module.exports = CorberWebpackPlugin;
