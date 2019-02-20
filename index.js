5c897271d93af0c7620e4101f977c3bcf98d882b
Kieran Brownlees <kieran@cloudscapelabs.com>
2019-02-21 09:28:45 +1300

    Make the filename relative rather than absolute so it works for the generic webpack case.
    https://github.com/isleofcode/corber/issues/584

--- index.js
+++ index.js
@@ -9,16 +9,17 @@ CorberWebpackPlugin.prototype.apply = function(compiler) {
   let context = compiler.context;
   let platform = process.argv.includes('--CORBER_PLATFORM=android')? 'android' : 'ios';
   let cdvAssets = cordovaAssetTree.getPaths(platform, './corber/cordova');
+  let basePath = path.join('./corber/cordova', cdvAssets.assetsPath)
 
   cdvAssets.files.forEach((file) => {
     if (file === 'plugins/**') {
       //need to build the tree until corber/cordova/utils/cordova-assets is upgraded
-      let plugins = glob.sync(path.join('./corber/cordova', cdvAssets.assetsPath, 'plugins/**/*.js'));
+      let plugins = glob.sync(path.join(basePath, 'plugins/**/*.js'));
       plugins.forEach((plugin) => {
         compiler.apply(new SingleEntryPlugin(
           context,
           path.join(process.cwd(), plugin),
-          plugin
+          plugin.replace(basePath + '/', '')
         ));
       });
     } else {
